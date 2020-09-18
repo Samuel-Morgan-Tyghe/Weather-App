df195a31cb5834b4ab661036c1a6791335ee1f2e
Samuel Morgan-Tyghe <sa>
2020-09-18 15:26:35 +0100

    OOP Typescript

--- src/Typescript/City.ts
+++ src/Typescript/City.ts
@@ -8,7 +8,7 @@ export function getCity() {
 return city
 }
 
-export function whichApi(city: string) {
+export function getApi(city: string) {
     
     document.getElementById("weatherLocation").innerHTML = city
     const api = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=ab57b33912fbd3d3015d3f296505d3a8'
--- src/Typescript/index.ts
+++ src/Typescript/index.ts
@@ -2,20 +2,28 @@
 
 // City button onClick -> axios GET -> handle request and construct
 // 7 WeatherDay objects -> render those 7 objects on the page
-
-import { weatherData } from './WeatherDay'
+import axios from 'axios'
+import { weatherInfo } from './WeatherDay copy'
 import { getCity } from './City'
-import { whichApi } from './City'
+import { getApi } from './City'
 
 
 
 document.querySelectorAll('.allButtons').forEach(item => {
-    item.addEventListener('click', event => {
-      //handle click 
-        // class WeatherReading is a 
-        var city = getCity()
-        var api = whichApi(city)
-        weatherData(api)
-        
+  item.addEventListener('click', event => {
+      main()
     })
-  })
+})
+  
+function main() {
+  
+  var city = getCity()
+  var api = getApi(city)
+  var iTimes = 0;
+  weatherInfo(api, iTimes)
+ 
+
+}
+
+
+   
